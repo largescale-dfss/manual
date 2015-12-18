e8bb31e3394804fe665492d370b51f8d6470c9c1
Abhi Agarwal <me@abhiagarwal.com>
2015-12-18 10:28:27 -0500

    Adding some functions to the bash script

--- init.sh
+++ init.sh
@@ -1,10 +1,22 @@
-if [ -d app ] ; then
-    rm -rf app
-fi
+function clean ()
+{
+    if [ -d app ] ; then
+        rm -rf app
+    fi
+}
 
-mkdir app
-git clone git@github.com:largescale-dfss/RPC-server-client.git app/RPC-server-client
-git clone git@github.com:largescale-dfss/distributed-file-system.git app/distributed-file-system
-git clone git@github.com:largescale-dfss/django-file-custom-storage.git app/django-file-custom-storage
-git clone git@github.com:largescale-dfss/manager.git app/manager
-git clone git@github.com:largescale-dfss/concept-demo.git app/concept-demo
+function clone()
+{
+    mkdir app
+    git clone git@github.com:largescale-dfss/RPC-server-client.git app/RPC-server-client
+    git clone git@github.com:largescale-dfss/distributed-file-system.git app/distributed-file-system
+    git clone git@github.com:largescale-dfss/django-file-custom-storage.git app/django-file-custom-storage
+    git clone git@github.com:largescale-dfss/manager.git app/manager
+    git clone git@github.com:largescale-dfss/concept-demo.git app/concept-demo
+}
+
+if [ $# -eq 0 ]
+  then
+    clean
+    clone
+fi
